b12184571cb3befb7bed4465393110cbdd0cb791
neelimaBonumaddi <neelimabonumaddi@gmail.com>
2025-05-01 20:10:28 +0530

    modules

--- hello.js
+++ hello.js
@@ -1,10 +1,18 @@
 const registerUser =require('./registration')
+const number =require('./registration')
+
 function hello()
 {
     console.log("hello from node js");
 }
 hello();
 
-registerUser.CheckForUser(
-    registerUser.registerUser()
-)
+ const isExisting = registerUser.CheckForUser() 
+ if(isExisting)
+ {
+    console.log("user is already there");
+ }
+ else
+ {   
+ registerUser.registerUser()
+ }
