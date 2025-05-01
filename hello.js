f248ac36348b8b6f8d7df7b6694c30e7c3889657
neelimaBonumaddi <neelimabonumaddi@gmail.com>
2025-05-01 22:40:13 +0530

    server

--- hello.js
+++ hello.js
@@ -1,5 +1,8 @@
-const registerUser =require('./registration')
-const number =require('./registration')
+const registerUser =require('./registration');
+const number =require('./registration');
+const os = require('os');
+const fs = require('fs');
+const express = require('express');
 
 function hello()
 {
@@ -16,3 +19,32 @@ hello();
  {   
  registerUser.registerUser()
  }
+
+ console.log(os.hostname());
+ console.log(os.freemem());
+ console.log(os.machine());
+ console.log(os.homedir());
+ console.log(os.loadavg());
+ console.log(os.EOL);
+
+ fs.writeFile('./sample.txt', 'Testing FS Modules',(err)=>{
+    if(err){
+        console.log("Error Occured While creating file");
+        return;
+    }
+    console.log("file created succesfully");
+ })
+
+ fs.readFile('./sample.txt', 'utf8', (err, data)=>{
+    if(err){
+        console.log("error occured");
+        return
+    }
+    console.log('data in file:', data);
+ })
+
+ const app=express();
+
+ app.listen(3000,()=>{
+    console.log("your server is running");
+ })
